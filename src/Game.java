ede3970198c3ca8fabd097800fbd5ba4adeed3d1
Nik Tretyak <nitre003on@gmail.com>
2021-03-04 18:01:48 +0100

    HUD, BASIC ITEM W/ GRAVITY
    HUD noch nicht perfekt, wird nicht jeden tick gelöscht, sondern nur drübergeschrieben

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+
 import javax.swing.JPanel;
 
 public class Game extends Window {
@@ -7,13 +9,18 @@ public class Game extends Window {
 	private static JPanel panel = new JPanel(); // Darauf kommen alle Objekte
 	// muss schon hier initialisiert werden, da es im Aufrufen von super() gebraucht
 	// wird.
-
+	
 	private Player player;
-
+	private Item item_t;
+	private HUD hud;
+	
 	// Delta time: siehe https://en.wikipedia.org/wiki/Delta_timing
 	private long dt;
 	private long lastT;
 
+	static int screenWidth = 1300;
+	static int screenHeight = 700;
+	
 	// Frames
 	int targetFPS = 60;
 	long frameTime = 1000 / targetFPS;
@@ -26,23 +33,33 @@ public class Game extends Window {
 
 	public Game() {
 
-		super("Epic Adventure Game", 0, 0, 1300, 700, panel);
+		super("Epic Adventure Game", 0, 0, screenWidth, screenHeight, panel);
 
-		// panel.setBackground(Color.RED);
+		panel.setBackground(Color.GRAY);
 
 		handler = new ObjectHandler();
 
 		player = new Player(handler);
 
+		hud = new HUD(player);
+		
 		this.addKeyListener(player);
 
 		panel.add(player);
 
 		player.setVisible(true);
 
+		handler.addObject(player);
+		
 		// player.setBounds(100, 100, 50, 80); // xPos, yPos, xSize, ySize
 
-		handler.addObject(player);
+		item_t = new Item(handler);
+		
+		panel.add(item_t);
+
+		item_t.setVisible(true);
+		
+		handler.addObject(item_t);
 		
 		wände[0]  = new Wand(200, 200, 100, 100);
 		wände[1]  = new Wand(100, 300, 300, 100);
@@ -63,8 +80,12 @@ public class Game extends Window {
 			lastT = System.nanoTime(); // delta time
 
 			handler.tick(dt);
-
+			
+			hud.tick();
+			
 			handler.renderAll();
+			
+			hud.render(getGraphics());
 
 			if (frameTime - ((System.currentTimeMillis() - timer)) > 0) {
 				try {
@@ -86,4 +107,13 @@ public class Game extends Window {
 		}
 	}
 
+	public static float clamp(float var, float min, float max) {
+	    if(var>=max) {
+	        return var = max;
+	      }else if(var<=min) {                                    //durch diese Methode wird var nicht ausserhalb der Grenzen von min un max gelassen
+	          return var = min;
+	        }else {
+	          return var;
+	        }
+	    }
 }
