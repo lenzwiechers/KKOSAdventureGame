f945d7b88136ba91b51527ff4c8a95a3300dbcb9
Unknown <unknown@example.com>
2021-03-13 04:36:52 +0100

    Game updated

--- src/Game.java
+++ src/Game.java
@@ -13,7 +13,6 @@ public class Game extends Window {
 	// wird.
 
 	private Player player;
-	private Item item_t;
 	private HUD hud;
 
 	// Delta time: siehe https://en.wikipedia.org/wiki/Delta_timing
@@ -30,10 +29,6 @@ public class Game extends Window {
 	long timer;
 
 	ObjectHandler handler;
-
-	Wand waende[] = new Wand[5];
-	
-	Door door1, door2, door3, door4;
 	
 	Camera cam;
 
@@ -43,10 +38,10 @@ public class Game extends Window {
 
 		panel.setBackground(Color.GRAY);
 
-		handler = new ObjectHandler();
+		handler = new ObjectHandler(panel);
 
 		player = new Player(handler, this, panel);
-		player.setPos('x', (screenWidth / 2) - (player.getSize('x') / 2));
+		player.setPos('x', (500) - (player.getSize('x') / 2));
 		player.setPos('y', (screenHeight / 2) - (player.getSize('y') / 2));
 		
 		System.out.println(player.getPos('x'));
@@ -62,45 +57,6 @@ public class Game extends Window {
 		player.setVisible(true);
 
 		handler.addObject(player);
-
-		item_t = new Item(handler, player, panel);
-		
-		panel.add(item_t);
-
-		item_t.setVisible(true);
-
-		handler.addObject(item_t);
-
-		waende[0] = new Wand(200, 200, 100, 100, handler);
-		waende[1] = new Wand(100, 300, 300, 100, handler);
-		waende[2] = new Wand(400, 500, 1000, 100, handler);
-		waende[3] = new Wand(1000, 400, 100, 100, handler);
-		waende[4] = new Wand(500, 400, 100, 100, handler);
-
-		for (int i = 0; i < waende.length; i++) {
-			handler.addObject(waende[i]);
-			panel.add(waende[i]);
-		}
-		
-		door1 = new Door(200, 100, 100, 100, handler);
-		door2 = new Door (800, 400, 100, 100, handler);
-		
-		door3 = new Door(2000, 200, 100, 100, handler);
-		door4 = new Door (1100, 100, 100, 100, handler);
-		
-		handler.addObject(door1);;
-		handler.addObject(door2);
-		panel.add(door1);
-		panel.add(door2);
-		
-		handler.addObject(door3);;
-		handler.addObject(door4);
-		panel.add(door3);
-		panel.add(door4);
-		
-		door1.connectExit(door2);
-		
-		door3.connectExit(door4);
 		
 		player.render();
 		
@@ -112,8 +68,14 @@ public class Game extends Window {
 		enemy1.setSize('x', 100);
 		enemy1.setSize('y', 100);
 		
+		generateMap.generate(panel, handler);
+		
 		lastT = System.nanoTime(); // delta time
 
+		run();
+	}
+	
+	public void run() {
 		while (true) {
 			timer = System.currentTimeMillis();
 
