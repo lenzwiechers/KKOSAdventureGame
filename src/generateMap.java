21c95d384be1452e69caa34e10b3fce9d05b7314
Hier0nymus <karl@your-memory.de>
2021-03-15 18:50:34 +0100

    map angefangen

--- src/generateMap.java
+++ src/generateMap.java
@@ -9,6 +9,16 @@ public abstract class generateMap {
 
 	public static void generate(ObjectHandler handler) {
 		generateStartRoom(handler);
+		
+		generateRoom1(handler);
+		
+		generateRoom2(handler);
+		
+		generateRoom3(handler);
+		
+		generateBossRoom(handler);
+		
+		
 
 		for (int i = 0; i < waende.size(); i++) {
 			handler.addObject(waende.get(i));
@@ -26,7 +36,47 @@ public abstract class generateMap {
 
 	public static void generateStartRoom(ObjectHandler handler) {
 		// Startraum Generierung
-
+		
+		//Grenzen: 0, -1000, 6000, 1000
+		
+		waende.add(new Wand(0, 1000, 1000, 1000, handler));
+		waende.add(new Wand(1000, 1000, 1000, 1000, handler));
+		waende.add(new Wand(-1000, 1000, 1000, 1000, handler));
+		waende.add(new Wand(-1000, 0, 1000, 1000, handler));
+		waende.add(new Wand(-1000, -1000, 1000, 1000, handler));
+		waende.add(new Wand(-1000, -2000, 1000, 1000, handler));
+		waende.add(new Wand(0, -2000, 1000, 1000, handler));
+		waende.add(new Wand(1000, -2000, 1000, 1000, handler));
+		
+		for (int i = 0; i < 5; i++) {
+			waende.add(new Wand(2000 + 1000*i, 1000, 1000, 1000, handler));
+			waende.add(new Wand(2000 + 1000*i, -2000, 1000, 1000, handler));
+		}
+		
+		waende.add(new Wand(6000, 0, 1000, 1000, handler));
+		waende.add(new Wand(6000, -1000, 1000, 1000, handler));
+		
+		waende.add(new Wand(500, 0, 50, 1000, handler));
+		waende.add(new Wand(400, 200, 100, 20, handler));
+		waende.add(new Wand(400, 600, 100, 20, handler));
+		waende.add(new Wand(0, 400, 100, 20, handler));
+		waende.add(new Wand(0, 800, 100, 20, handler));
+		waende.add(new Wand(0, 0, 100, 20, handler));
+		
+		waende.add(new Wand(550, 0, 700, 1000, handler));
+		
+		waende.add(new Wand(750, -150, 30, 150, handler));
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+/*
 		waende.add(new Wand(0, -200, 100, 900, handler));
 		waende.add(new Wand(0, 700, 1000, 100, handler));
 		waende.add(new Wand(1200, 700, 1000, 100, handler));
@@ -53,14 +103,39 @@ public abstract class generateMap {
 		tueren.add(door1);
 		tueren.add(door2);
 		door1.connectExit(door2);
-
-		Door door3 = new Door(500, 500, 100, 100, handler);
-
-		tueren.add(door3);
+		
+		*/
 	}
 	
-	public static void generateFloor1(ObjectHandler handler) {
+	public static void generateRoom1(ObjectHandler handler) {
 		// Ebene 1 Generierung
+		
+		waende.add(new Wand(0, 3000, 100, 1000, handler));
+		waende.add(new Wand(0, 4000, 1000, 100, handler));
+		
+		Door door4 = new Door(200, 3900, 100, 100, handler);
+		Door door3 = new Door(1800, 300, 100, 100, handler);
+		tueren.add(door3);
+		tueren.add(door4);
+		door3.connectExit(door4);
+		
+		
+	}
+	
+	public static void generateRoom2(ObjectHandler handler) {
+		// Ebene 2 Generierung
+		
+		
+	}
+	
+	public static void generateRoom3(ObjectHandler handler) {
+		// Ebene 3 Generierung
+		
+		
+	}
+	
+	public static void generateBossRoom(ObjectHandler handler) {
+		// Bossebene Generierung
 		
 		
 	}
